90bd1c489bc8ae4d412bfb59c0def7e43721853f
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-21 16:50:02 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install python3")
+    os.system("pkg install Conda")
+    os.system("pip install flood")
+   
+
+elif c == "1":
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install python3")
+    os.system("pkg install Conda")
+    os.system("pip install flood")
+   
+print("Done.")
