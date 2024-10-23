b97d7a562540bfdd6fceea388711baf5c8545543
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-23 12:15:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pip install requests")
-    os.system("pkg install flood")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install requests")
+    os.system("apt install flood")
    
 
 elif c == "1":
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pip install requests")
-    os.system("pkg install flood")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install requests")
+    os.system("apt install flood")
    
 print("Done.")
